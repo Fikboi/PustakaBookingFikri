df5f9df7460cdb071588a7148cb415d37596f3b7
Fikboi <f.haikal224@gmail.com>
2022-05-30 00:30:23 +0700

    Project Fikri

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Mohammad Fikri Haikal
+ Saya tingga di daerah Tangerang
+ olah raga yang saya sukai adalah
+ Futsal";
+ }
+}
